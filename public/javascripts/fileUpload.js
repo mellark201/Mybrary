014737cda16de84f0395dcd67b99234263e34768
mellark201 <skjha1177@gmail.com>
2020-06-16 14:08:23 +0530

    Normal Changes

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,26 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginFileEncode,
-    FilePondPluginImageResize,
-)
+const rootStyle = window.getComputedStyle(document.documentElement)
+if(rootStyle.getPropertyValue('--book-cover-width-large') != null && rootStyle.getPropertyValue('--book-cover-width-large') != "") {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready);
+}
+console.log('Hello');
+function ready() {
+    const coverWidth = parseFloat(rootStyle.getPropertyValue('--book-cover-width-large'))
+    const coverRatio = parseFloat(rootStyle.getPropertyValue('--book-cover-aspect-ratio'))
+    console.log(coverWidth);
+    const coverHeight = coverWidth / coverRatio
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginFileEncode,
+        FilePondPluginImageResize,
+    )
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150,
-})
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight,
+    })
+}
 
 FilePond.parse(document.body);
